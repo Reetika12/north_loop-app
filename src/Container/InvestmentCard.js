6d663f9502dfc41dc2a0840a143576f96dd4279f
reetika <reetika.singh@byjus.com>
2021-04-09 12:24:27 +0530

    first commit of set up

---
+++ src/Container/InvestmentCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../Styles/InvestmentCard.css'
+
+class InvestmentCard extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h2>Investing in Mutual Funds Simplified.</h2>
+            </div>
+        )
+    }
+}
+
+export default InvestmentCard
